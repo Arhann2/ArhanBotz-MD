b9b70277d782093e037d034b2542e990e7b1ba84
Arhann2 <107976855+Arhann2@users.noreply.github.com>
2022-06-27 13:05:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
+  ['6281222746323'],
   ['62895330379186'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
