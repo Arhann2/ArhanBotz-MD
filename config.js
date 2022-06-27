cc584d078ca9203ee3c9644432ebc9932d5aa061
Arhann2 <107976855+Arhann2@users.noreply.github.com>
2022-06-27 13:07:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let chalk = require('chalk')
 
 global.owner = [
   ['6281222746323'],
-  ['62895330379186'],
+  ['6282396055503'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@arhan_bot'
+  var sticker_author = 'Arhan'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© Arhan'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
