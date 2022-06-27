c5b75e4ca8f771c5da7d3c8bd15a61a36132eeac
Arhann2 <107976855+Arhann2@users.noreply.github.com>
2022-06-27 14:30:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@arhan_bot'
-  var sticker_author = 'Arhan'
+  var sticker_name = '@Annisa_bot'
+  var sticker_author = 'Annisa'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Arhan'
+global.wm = '© Annisa'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
